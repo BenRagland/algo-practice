112961de0d9151432031684dabf5d1adb6edbd99
Benjamin <Officialbenragland@gmail.com>
2024-02-27 02:01:58 -0500

    create render and fetch from json server. Has Refresh Bug

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", () =>{
     const form = document.querySelector('form')
+    
+    // Post not working with the renderCard function and console.log of server data response
+    //ins't staying on the page. -->Maybe a refresh error with e.default()?
+
+    //need to fetch json-server when page loads. Can test renderCard funciton then.
+
+    //fetch respons cycle is not correct . Submit button should not page refesh
+
+
+    fetch('http://localhost:3000/algos')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach( algo => renderCard(algo) )
+    })
+    // handle fetch errors
+    
+
     form.addEventListener("submit",(e)=>{
         e.preventDefault()
         const algoName = e.target["algo-name"].value
@@ -32,13 +49,33 @@ document.addEventListener("DOMContentLoaded", () =>{
                 alert("server error", res)
             }
         })
-        .then(card => console.log(card))
-        
+        .then(cardData => {
+            console.log(cardData)
+            // renderCard(cardData)
+        })
+                   
         .catch(  err => alert("No Server Response", err))
         
 
     })
 
+    function renderCard(cardData){
+        const solutionsArea = document.querySelector(".solutions-area")
+        const algoCard = document.createElement("div")
+        algoCard.className = "card-body"
+        algoCard.innerHTML = ` 
+        <h4>Algorithm Name</h4>
+            <p>${cardData.algoName}</p>
+            <h6>What the Algorithm Does</h6>
+            <p>${cardData.algoExplain}</p>
+            <h6>Pseudo Code</h6>
+            <p>${cardData.pseudo}</p>
+            <h6>Javascript</h6>
+            <p>${cardData.java}</p>
+        
+        `
+        solutionsArea.appendChild(algoCard)
+    }
 
     
 
